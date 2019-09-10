a03223f443fcf82f69f84c389e9055c6f4af276b
bitsapien <bitsapien@gmail.com>
2019-09-10 22:16:36 +0530

    Add doom emacs 😱

---
+++ doom-emacs/.doom.d/config.el
@@ -0,0 +1,11 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+(setq mac-option-key-is-meta nil                        ;; Remapping Meta to CMD
+      mac-command-key-is-meta t
+      mac-command-modifier 'meta
+      mac-option-modifier 'none
+      projectile-project-search-path '("~/gitdisk/tw/") ;; Projectile search paths 🔍
+      indent-tabs-mode nil                              ;; No Tabs ⌨️
+      tab-width 4                                       ;; Cause tab widths matter
+      )

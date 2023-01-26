71e1e8cc318e7973bc80fe3a3bdca0f0bae962ea
Rahul C <3462960+bitsapien@users.noreply.github.com>
2023-01-26 00:45:21 +0000

    echo on server script

--- servers/setup_env.sh
+++ servers/setup_env.sh
@@ -4,5 +4,5 @@ apt-get install git-core
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 usermod -u bitsapien -s `which zsh`
 exit
-# change .zshrc to st theme `gallois`
+echo "change .zshrc to st theme `gallois`"
 # enjoy

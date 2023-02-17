29e34f424262e61fb5739cb754ba12f08a4c51bc
Rahul C <3462960+bitsapien@users.noreply.github.com>
2023-02-17 04:08:22 +0000

    [ServerSetup] Add package test

--- servers/setup_env.sh
+++ servers/setup_env.sh
@@ -1,8 +1,13 @@
 sudo su
-apt-get install zsh
-apt-get install git-core
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-usermod -u bitsapien -s `which zsh`
-exit
-echo "change .zshrc to st theme `gallois`"
+
+for package_name in zsh git-core 
+do 
+  echo $package_name
+done
+#apt-get install zsh
+#apt-get install git-core
+#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+#usermod -u bitsapien -s `which zsh`
+#exit
+#echo "change .zshrc to st theme `gallois`"
 # enjoy
